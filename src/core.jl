4df62e47791347f8bddc624b1967ca3642d82580
landinjm <landinjm@umich.edu>
2025-10-29 20:29:36 -0700

    ok gradients & hessians

--- src/core.jl
+++ src/core.jl
@@ -7,9 +7,93 @@ function time_derivative(u::Symbolics.Num, t::Symbolics.Num)
   return expand_derivatives(Differential(t)(u))
 end
 
+export spatial_gradient
+function spatial_gradient(u::Symbolics.Num, x::Symbolics.Num)
+  return expand_derivatives(Differential(x)(u))
+end
+function spatial_gradient(u::Symbolics.Num, x::Symbolics.Num, y::Symbolics.Num)
+  return [spatial_gradient(u, x) spatial_gradient(u, y)]
+end
+function spatial_gradient(
+  u::Symbolics.Num,
+  x::Symbolics.Num,
+  y::Symbolics.Num,
+  z::Symbolics.Num,
+)
+  return [spatial_gradient(u, x) spatial_gradient(u, y) spatial_gradient(u, z)]
+end
+
+export spatial_hessian
+function spatial_hessian(u::Symbolics.Num, x::Symbolics.Num)
+  return spatial_gradient(spatial_gradient(u, x), x)
+end
+function spatial_hessian(u::Symbolics.Num, x::Symbolics.Num, y::Symbolics.Num)
+  return [
+    spatial_hessian(u, x) spatial_gradient(spatial_gradient(u, x), y)
+    spatial_gradient(spatial_gradient(u, y), x) spatial_hessian(u, y)
+  ]
+end
+function spatial_hessian(
+  u::Symbolics.Num,
+  x::Symbolics.Num,
+  y::Symbolics.Num,
+  z::Symbolics.Num,
+)
+  return [
+    spatial_hessian(u, x) spatial_gradient(spatial_gradient(u, x), y) spatial_gradient(spatial_gradient(u, x), z)
+    spatial_gradient(spatial_gradient(u, y), x) spatial_hessian(u, y) spatial_gradient(spatial_gradient(u, y), z)
+    spatial_gradient(spatial_gradient(u, z), x) spatial_gradient(spatial_gradient(u, z), y) spatial_hessian(u, z)
+  ]
+end
+
 export symbolically_equal
 function symbolically_equal(expr1::Symbolics.Num, expr2::Symbolics.Num)
   return simplify(expr1 - expr2) == 0
+end
+function symbolically_equal(expr1::Symbolics.Num, expr2::Real)
+  return simplify(expr1 - Symbolics.Num(expr2)) == 0
+end
+function symbolically_equal(expr1::Symbolics.Num, expr2::Integer)
+  return simplify(expr1 - Symbolics.Num(expr2)) == 0
+end
+function symbolically_equal(
+  expr1::Matrix{Symbolics.Num},
+  expr2::Matrix{Symbolics.Num},
+)
+  if size(expr1) != size(expr2)
+    return false
+  end
+  for i in eachindex(expr1)
+    if !symbolically_equal(expr1[i], expr2[i])
+      return false
+    end
+  end
+
+  return true
+end
+function symbolically_equal(expr1::Matrix{Symbolics.Num}, expr2::Matrix{<:Real})
+  if size(expr1) != size(expr2)
+    return false
+  end
+  for i in eachindex(expr1)
+    if !symbolically_equal(expr1[i], expr2[i])
+      return false
+    end
+  end
+
+  return true
+end
+function symbolically_equal(expr1::Matrix{Symbolics.Num}, expr2::Matrix{<:Integer})
+  if size(expr1) != size(expr2)
+    return false
+  end
+  for i in eachindex(expr1)
+    if !symbolically_equal(expr1[i], expr2[i])
+      return false
+    end
+  end
+
+  return true
 end
 
 export numerically_equal
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Varis
 using Test
 
 @testset "Varis.jl" begin
-  @testset "Time derivative" begin
+  @testset "Simple time derivative" begin
     # Define symbolic variables and equation
     @vars t x
     u = sin(x) * exp(-t)
@@ -14,5 +14,71 @@ using Test
 
     # Test that the time derivative is numerically equal
     @test Varis.Core.numerically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial gradient 1D" begin
+    # Define symbolic variables and equation
+    @vars x u(x)
+    u = x^2
+    calculated = Varis.Core.spatial_gradient(u, x)
+    expected = 2x
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial gradient 2D" begin
+    # Define symbolic variables and equation
+    @vars x y u(x, y)
+    u = x^2 + y^2
+    calculated = Varis.Core.spatial_gradient(u, x, y)
+    expected = [2x 2y]
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial gradient 3D" begin
+    # Define symbolic variables and equation
+    @vars x y z u(x, y, z)
+    u = x^2 + y^2 + z^2
+    calculated = Varis.Core.spatial_gradient(u, x, y, z)
+    expected = [2x 2y 2z]
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial hessian 1D" begin
+    # Define symbolic variables and equation
+    @vars x u(x)
+    u = x^2
+    calculated = Varis.Core.spatial_hessian(u, x)
+    expected = 2
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial hessian 2D" begin
+    # Define symbolic variables and equation
+    @vars x y u(x, y)
+    u = x^2 + y^2
+    calculated = Varis.Core.spatial_hessian(u, x, y)
+    expected = [2 0; 0 2]
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
+  end
+
+  @testset "Simple spatial hessian 3D" begin
+    # Define symbolic variables and equation
+    @vars x y z u(x, y, z)
+    u = x^2 + y^2 + z^2
+    calculated = Varis.Core.spatial_hessian(u, x, y, z)
+    expected = [2 0 0; 0 2 0; 0 0 2]
+
+    # Test that the spatial gradient is symbolically equal 
+    @test Varis.Core.symbolically_equal(calculated, expected)
   end
 end
